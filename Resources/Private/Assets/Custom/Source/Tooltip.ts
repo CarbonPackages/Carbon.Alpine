0f64b88b526525d9d861fe8fff22fa582c0783ab
Jon Uhlmann <account@uhlmann.pro>
2023-12-26 13:00:15 +0100

    Update: Tooltip

--- Resources/Private/Assets/Custom/Source/Tooltip.ts
+++ Resources/Private/Assets/Custom/Source/Tooltip.ts
@@ -1,33 +1,51 @@
 import tippy, { createSingleton } from "tippy.js";
 
-const delay = [500, 0];
-
-function content(element: Element) {
-    return element.getAttribute("aria-label") || element.getAttribute("title");
-}
+const delay: number | [number, number] = [500, 0];
 
 export default function (Alpine) {
     // Directive: x-tooltip
     Alpine.directive("tooltip", (element, { expression }) => {
-        // @ts-ignore
-        tippy(element, {
-            placement: expression || "top",
-            content: content(element),
-            delay,
-        });
+        createTippy(element, expression || "top");
     });
 
     // Directive: x-tooltips
     Alpine.directive("tooltips", (element, { expression }) => {
         const placement = expression || "top";
-        // @ts-ignore
-        const instances = [...element.querySelectorAll("[aria-label]")].map((element) =>
-            // @ts-ignore
-            tippy(element, { placement, content: content(element), delay }),
-        );
-        createSingleton(instances, {
-            delay: 500,
-            moveTransition: "transform 0.2s ease-out",
+        Alpine.bind(element, {
+            "x-init"() {
+                this.$nextTick(() => {
+                    const elements = [...element.querySelectorAll(":where([aria-label],[title])")];
+                    const instances = elements.map((element) => createTippy(element, placement));
+                    // @ts-ignore
+                    createSingleton(instances, {
+                        delay: 500,
+                        moveTransition: "transform 0.2s ease-out",
+                    });
+                });
+            },
         });
+    });
+}
+
+function content(element: Element) {
+    return element.getAttribute("aria-label") || element.getAttribute("title");
+}
+
+const dynamicContentPlugin = {
+    name: "dynamicContent",
+    fn: (instance) => ({
+        onShow() {
+            console.log(instance, instance.reference);
+            instance.setContent(content(instance.reference));
+        },
+    }),
+};
+
+function createTippy(element: Element, placement) {
+    return tippy(element, {
+        content: content(element),
+        placement,
+        delay,
+        plugins: [dynamicContentPlugin],
     });
 }
