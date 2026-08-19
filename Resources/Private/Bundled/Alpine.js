33870a1a6866303f4d792e18b8ea3d746bff8d82
Jon Uhlmann <account@uhlmann.pro>
2026-08-19 11:33:27 +0200

    Update: AlpineJS 3.16.2

--- Resources/Private/Bundled/Alpine.js
+++ Resources/Private/Bundled/Alpine.js
@@ -1,2 +1,2 @@
-import { Alpine_default } from "./chunk-KOXOLAWX.js";
+import { Alpine_default } from "./chunk-QKAOF3R4.js";
 export { Alpine_default as default };
--- Resources/Private/Bundled/Plugins/Persist.js
+++ Resources/Private/Bundled/Plugins/Persist.js
@@ -1,4 +1,4 @@
-// node_modules/.pnpm/@alpinejs+persist@3.16.1/node_modules/@alpinejs/persist/dist/module.esm.js
+// node_modules/.pnpm/@alpinejs+persist@3.16.2/node_modules/@alpinejs/persist/dist/module.esm.js
 function src_default(Alpine) {
     let persist = () => {
         let alias;
@@ -10,7 +10,7 @@ function src_default(Alpine) {
             console.warn("Alpine: $persist is using temporary storage since localStorage is unavailable.");
             let dummy = /* @__PURE__ */ new Map();
             storage = {
-                getItem: dummy.get.bind(dummy),
+                getItem: (key) => (dummy.has(key) ? dummy.get(key) : null),
                 setItem: dummy.set.bind(dummy),
             };
         }
@@ -52,7 +52,8 @@ function src_default(Alpine) {
     };
 }
 function storageHas(key, storage) {
-    return storage.getItem(key) !== null;
+    let value = storage.getItem(key);
+    return value !== null && value !== void 0;
 }
 function storageGet(key, storage) {
     let value = storage.getItem(key);
@@ -60,6 +61,10 @@ function storageGet(key, storage) {
     return JSON.parse(value);
 }
 function storageSet(key, value, storage) {
+    if (value === void 0) {
+        storage.removeItem?.(key);
+        return;
+    }
     storage.setItem(key, JSON.stringify(value));
 }
 var module_default = src_default;
